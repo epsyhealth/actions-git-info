47177dcc025de86010886f090daf68943f1796cc
Karol Czeryna <k@e-dot.uk>
2021-07-19 11:25:22 +0100

    Move to GrapQL query for checking last 10 deployments

--- git_info/cli.py
+++ git_info/cli.py
@@ -2,16 +2,15 @@ import os
 import re
 
 import click
+import requests
 from git import Repo, BadName
 from github import Github, UnknownObjectException
-
-client = Github(os.getenv("GITHUB_TOKEN"))
-
 
 def get_pr_info(pr_url):
     if not pr_url:
         raise click.exceptions.ClickException("Invalid PR URL")
-
+    
+    client = Github(os.getenv("GITHUB_TOKEN"))
     regex = r"https://(api\.|)github\.com/(repos\/|)(?P<repo>.*?)/pulls?/(?P<pull_id>\d+)"
     matches = re.match(regex, pr_url)
 
@@ -45,14 +44,32 @@ def get_git_info(work_dir, variables):
         previous_tag = tags[-1]
 
     # check deployments if there is no tag
+    repo = os.getenv("GITHUB_REPOSITORY").split('/')
+    stage = os.getenv("STAGE")
+    graphql_query = f'''
+        {{
+        repository(owner: "{repo[0]}", name: "{repo[1]}") {{  
+        deployments(environments: ["{stage}"], first: 10) {{  
+        edges {{  
+        cursor  
+        node {{
+        state
+        commit {{
+        oid  
+        }}  
+        }}  
+        }}  
+        }}  
+        }}
+        }}'''
+    token = os.getenv("GITHUB_TOKEN")
+    headers = {"Authorization": f"bearer {token}"}
+    request = requests.post("https://api.github.com/graphql", json={"query": graphql_query}, headers=headers)
+    data = request.json()
     previous_deployment_sha = None
-    remote_repo = client.get_repo(os.getenv("GITHUB_REPOSITORY"))
-    deployments = remote_repo.get_deployments(environment=os.getenv("STAGE"))
-    deployments = deployments[:10]  # limit to last 10 deployments
-    for i in deployments:
-        for x in remote_repo.get_deployment(i.id).get_statuses():
-            if remote_repo.get_deployment(i.id).get_status(x.id).state == "success" and not previous_deployment_sha:
-                previous_deployment_sha = remote_repo.get_deployment(i.id).sha
+    for deployment in data["data"]["repository"]["deployments"]["edges"]:
+        if deployment["node"]["state"] == "ACTIVE":
+            previous_deployment_sha = deployment["node"]["commit"]["oid"]
     # We didn't find any deployments or there is none? use first git commit instead!
     if previous_deployment_sha is None:
         commits_in_master = repo.iter_commits(rev=repo.branches.master)
